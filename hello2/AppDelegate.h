9d260e2fe7ef3b452e799b17422cc9e5340b0612
姚健 <YaoJian@yjdemacbook-air.local>
2016-03-13 23:36:36 +0800

    Initial Commit

---
+++ hello2/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  hello2
+//
+//  Created by YJ on 16/3/13.
+//  Copyright © 2016年 YJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ hello2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  hello2
+//
+//  Created by YJ on 16/3/13.
+//  Copyright © 2016年 YJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
